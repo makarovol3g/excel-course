366b2e7bc55d31ace672eed28564ea0d4d17aa1e
unknown <makarovol3g@gmail.com>
2020-07-31 01:06:12 +0300

    added adding of event listeners, need to realize removing of it

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,9 +1,17 @@
 import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
+    constructor($root, options = {}) {
+        super($root, options.listeners)
+        this.name = options.name || ''
+    }
     // Возвращает HTML шаблон компонента
     toHTML() {
         return ''
+    }
+
+    init() {
+        this.initDOMListeners()
     }
 }
 
