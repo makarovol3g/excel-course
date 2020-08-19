a85f6c1708d5b62eb5c0c421d62a22a141c36dd9
Makarov Oleg <makarovol3g@gmail.com>
2020-08-19 13:46:41 +0300

    Routing implemented

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,6 +1,5 @@
-import {storage} from '@core/utils'
-import {defaultStyles} from '@/constants'
-import {defaultTitle} from '@/constants'
+import {defaultStyles, defaultTitle} from '@/constants'
+import {clone} from '@core/utils'
 
 export const defaultState = {
     title: defaultTitle,
@@ -9,7 +8,8 @@ export const defaultState = {
     dataState: {},
     stylesState: {},
     currentText: '',
-    currentStyles: defaultStyles
+    currentStyles: defaultStyles,
+    openedDate: new Date().toJSON()
 }
 
 const normalize = state => ({
@@ -18,6 +18,6 @@ const normalize = state => ({
     currentText: ''
 })
 
-export const initialState = storage('excel-state')
-    ? normalize(storage('excel-state'))
-    : defaultState
+export function normalizeInitialState(state) {
+    return state ? normalize(state) : clone(defaultState)
+}
